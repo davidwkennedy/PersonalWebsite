c19fff1f11738d1146fc679bc7c001d90473dd98
Dave <davidwkennedy@gmail.com>
2013-09-10 19:46:44 -0700

    Various incremental updates - including proper resizing of background image.

---
+++ static/script/ResizeBackground.js
@@ -0,0 +1,25 @@
+﻿function DetermineBackgroundSize() {
+    $(window).load(function () {
+
+        var theWindow = $(window),
+            $bg = $("#backgroundImage"),
+            aspectRatio = $bg.width() / $bg.height();
+
+        function resizeBg() {
+            if ((theWindow.width() / theWindow.height()) < aspectRatio) {
+                $bg
+                    .removeClass()
+                    .addClass('bgheight');
+            } else {
+                $bg
+                    .removeClass()
+                    .addClass('bgwidth');
+            }
+        }
+
+        theWindow.resize(resizeBg).trigger("resize");
+    });
+
+
+}
+        
